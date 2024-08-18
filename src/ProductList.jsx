f0dcf180f55eda688c0570d3af546f70a550b92a
Purple <goncalosroxo@gmail.com>
2024-08-18 13:12:24 -0400

    fix 404 on git pages

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -294,6 +294,11 @@ function ProductList() {
     setShowCart(false); // Hide the cart when navigating to About Us
   };
 
+  const handleHomeClick = (e) => {
+    e.preventDefault();
+    window.location.reload();
+  };
+
   const handleContinueShopping = (e) => {
     e.preventDefault();
     setShowCart(false);
@@ -316,7 +321,7 @@ function ProductList() {
               src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png"
               alt=""
             />
-            <a href="/" style={{ textDecoration: "none" }}>
+            <a href="#" onClick={(e) => handleHomeClick(e)} style={{ textDecoration: "none" }}>
               <div>
                 <h3 style={{ color: "white" }}>Paradise Nursery</h3>
                 <i style={{ color: "white" }}>Where Green Meets Serenity</i>
